c940c2e40728eadbbf78bbaeb35f02090cdc8dd8
Daniel Schädler <32635600+Malaber@users.noreply.github.com>
2019-12-03 14:56:51 +0100

    Rearrange Code in 02/1 to simplify 02/2

--- code/02.rb
+++ code/02.rb
@@ -1,36 +1,42 @@
 require_relative '../input'
 lines = get_lines $PROGRAM_NAME
 
-codes = lines.first.split(',').map!(&:to_i)
+def calculate_output(lines, noun, verb)
+  codes = lines.first.split(',').map!(&:to_i)
 
 # restore
-codes[1] = 12
-codes[2] = 2
+  codes[1] = noun
+  codes[2] = verb
 
-running = true
-current = 0
+  running = true
+  current = 0
 
-while running
-  case codes[current]
-  when 1
-    # addition
-    dig1 = codes[codes[current + 1]]
-    dig2 = codes[codes[current + 2]]
-    codes[codes[current + 3]] = dig1 + dig2
-  when 2
-    # multiplication
-    dig1 = codes[codes[current + 1]]
-    dig2 = codes[codes[current + 2]]
-    codes[codes[current + 3]] = dig1 * dig2
-  when 99
-    puts 'Program halted.'
-    running = false
-  else
-    puts 'The ship computer bursts into flames.'
-    running = false
+  while running
+    case codes[current]
+    when 1
+      # addition
+      dig1 = codes[codes[current + 1]]
+      dig2 = codes[codes[current + 2]]
+      codes[codes[current + 3]] = dig1 + dig2
+    when 2
+      # multiplication
+      dig1 = codes[codes[current + 1]]
+      dig2 = codes[codes[current + 2]]
+      codes[codes[current + 3]] = dig1 * dig2
+    when 99
+      running = false
+    else
+      puts 'The ship computer bursts into flames.'
+      running = false
+    end
+    current += 4
   end
-  current += 4
+
+  output = codes[0]
 end
 
-print 'Code at Position 0: '
-puts codes[0]
+output = calculate_output(lines, 12, 2)
+
+print 'P1: Code at Position 0: '
+puts output
+
