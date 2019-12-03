1135e3aeb155cd754fb1f3d5b31bc4e4a5696ea1
Daniel Schädler <32635600+Malaber@users.noreply.github.com>
2019-12-03 13:35:00 +0100

    Add puzzle 01/2

--- 01.rb
+++ 01.rb
@@ -1,6 +1,16 @@
 require_relative 'input'
 lines = get_lines $0
 
+def fuelfuel(fuel)
+  required_fuel = ((fuel / 3).floor) - 2
+  if required_fuel > 0
+    return fuelfuel(required_fuel) + required_fuel
+  else
+    return 0
+  end
+end
+
+
 allfuel = 0
 lines.each do |line|
   mass = line.to_i
@@ -8,4 +18,17 @@ lines.each do |line|
   allfuel += fuel
 end
 
+print "1: "
 puts allfuel
+
+allfuel_with_fuelfuel = 0
+lines.each do |line|
+  mass = line.to_i
+  fuel = ((mass / 3).floor) - 2
+  allfuel_with_fuelfuel += fuel
+
+  allfuel_with_fuelfuel += fuelfuel(fuel)
+end
+
+print "2: "
+puts allfuel_with_fuelfuel
