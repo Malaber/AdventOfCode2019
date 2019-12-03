c2f10f5e6ea3b566431da846ccb7f7085db8caa7
Daniel Schädler <32635600+Malaber@users.noreply.github.com>
2019-12-03 15:04:00 +0100

    Add puzzle 02/2

--- code/02.rb
+++ code/02.rb
@@ -40,3 +40,12 @@ output = calculate_output(lines, 12, 2)
 print 'P1: Code at Position 0: '
 puts output
 
+100.times do |noun|
+  100.times do |verb|
+    output = calculate_output(lines, noun, verb)
+    if output == 19690720
+      print "P2: 19690720 found: "
+      puts 100 * noun + verb
+    end
+  end
+end
