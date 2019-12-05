e4f8e88c91d675d2f5d31722918990b254b5c25c
Malaber <git@schaedler.rocks>
2019-12-05 12:53:40 +0100

    Move intcode_computer to extra file

--- code/02.rb
+++ code/02.rb
@@ -1,39 +1,7 @@
 require_relative '../input'
 lines = get_lines $PROGRAM_NAME
 
-def calculate_output(lines, noun, verb)
-  codes = lines.first.split(',').map!(&:to_i)
-
-# restore
-  codes[1] = noun
-  codes[2] = verb
-
-  running = true
-  current = 0
-
-  while running
-    case codes[current]
-    when 1
-      # addition
-      dig1 = codes[codes[current + 1]]
-      dig2 = codes[codes[current + 2]]
-      codes[codes[current + 3]] = dig1 + dig2
-    when 2
-      # multiplication
-      dig1 = codes[codes[current + 1]]
-      dig2 = codes[codes[current + 2]]
-      codes[codes[current + 3]] = dig1 * dig2
-    when 99
-      running = false
-    else
-      puts 'The ship computer bursts into flames.'
-      running = false
-    end
-    current += 4
-  end
-
-  output = codes[0]
-end
+require_relative 'intcode_computer.rb'
 
 output = calculate_output(lines, 12, 2)
 
---
+++ code/intcode_computer.rb
@@ -0,0 +1,99 @@
+def get_next_param_mode!(digits)
+  digit = digits.pop
+  digit.nil? ? 0 : digit
+end
+
+def get_parameter_value(position, mode, codes)
+  case mode
+  when 0
+    return codes[codes[position]].to_i
+  when 1
+    return codes[position].to_i
+  else
+    raise "OP Parameter Mode '#{mode}' not allowed"
+  end
+end
+
+def calculate_output(lines, noun = nil, verb = nil, input = nil)
+  codes = lines.first.split(',').map!(&:to_i)
+
+# restore
+  codes[1] = noun unless noun.nil?
+  codes[2] = verb unless verb.nil?
+
+  running = true
+  current = 0
+
+  while running
+    op_code_digits = codes[current].digits.reverse
+    op_code = op_code_digits.pop(2).join('').to_i
+
+    p1_mode = get_next_param_mode!(op_code_digits)
+    p2_mode = get_next_param_mode!(op_code_digits)
+    p3_mode = get_next_param_mode!(op_code_digits)
+
+    case op_code
+    when 1
+      # addition
+      dig1 = get_parameter_value(current + 1, p1_mode, codes)
+      dig2 = get_parameter_value(current + 2, p2_mode, codes)
+      codes[codes[current + 3]] = dig1 + dig2
+      current += 4
+    when 2
+      # multiplication
+      dig1 = get_parameter_value(current + 1, p1_mode, codes)
+      dig2 = get_parameter_value(current + 2, p2_mode, codes)
+      codes[codes[current + 3]] = dig1 * dig2
+      current += 4
+    when 3
+      # input
+      puts "Input Code: #{input}"
+      codes[codes[current + 1]] = input
+      current += 2
+    when 4
+      # output
+      puts "Output Code:"
+      puts get_parameter_value(current + 1, p1_mode, codes)
+      current += 2
+    when 5
+      # jump if true
+      p1 = get_parameter_value(current + 1, p1_mode, codes)
+      p2 = get_parameter_value(current + 2, p2_mode, codes)
+      if p1.zero?
+        current +=3
+      else
+        current = p2
+      end
+    when 6
+      # jump if false
+      p1 = get_parameter_value(current + 1, p1_mode, codes)
+      p2 = get_parameter_value(current + 2, p2_mode, codes)
+      if p1.zero?
+        current = p2
+      else
+        current +=3
+      end
+    when 7
+      # less than
+      dig1 = get_parameter_value(current + 1, p1_mode, codes)
+      dig2 = get_parameter_value(current + 2, p2_mode, codes)
+      codes[codes[current + 3]] = (dig1 < dig2)? 1 : 0
+      current += 4
+    when 8
+      # equals
+      dig1 = get_parameter_value(current + 1, p1_mode, codes)
+      dig2 = get_parameter_value(current + 2, p2_mode, codes)
+      codes[codes[current + 3]] = (dig1 == dig2)? 1 : 0
+      current += 4
+    when 99
+      current += 1
+      running = false
+    else
+      puts 'The ship computer bursts into flames.'
+      current += 1
+      running = false
+    end
+  end
+
+  codes[0]
+end
