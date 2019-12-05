bdc6f9d8ac7205d1936ee24dce33ebd539d5a3ec
Malaber <git@schaedler.rocks>
2019-12-05 08:24:35 +0100

    Copy 02 to 05

---
+++ code/05.rb
@@ -0,0 +1,51 @@
+require_relative '../input'
+lines = get_lines $PROGRAM_NAME
+
+def calculate_output(lines, noun, verb)
+  codes = lines.first.split(',').map!(&:to_i)
+
+# restore
+  codes[1] = noun
+  codes[2] = verb
+
+  running = true
+  current = 0
+
+  while running
+    case codes[current]
+    when 1
+      # addition
+      dig1 = codes[codes[current + 1]]
+      dig2 = codes[codes[current + 2]]
+      codes[codes[current + 3]] = dig1 + dig2
+    when 2
+      # multiplication
+      dig1 = codes[codes[current + 1]]
+      dig2 = codes[codes[current + 2]]
+      codes[codes[current + 3]] = dig1 * dig2
+    when 99
+      running = false
+    else
+      puts 'The ship computer bursts into flames.'
+      running = false
+    end
+    current += 4
+  end
+
+  output = codes[0]
+end
+
+output = calculate_output(lines, 12, 2)
+
+print 'P1: Code at Position 0: '
+puts output
+
+100.times do |noun|
+  100.times do |verb|
+    output = calculate_output(lines, noun, verb)
+    if output == 19690720
+      print "P2: 19690720 found: "
+      puts 100 * noun + verb
+    end
+  end
+end
