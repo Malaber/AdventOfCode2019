aacd4d4cf8ad0cba5329df8afa3c050fcc74e493
Malaber <git@schaedler.rocks>
2019-12-06 12:33:34 +0100

    Cleanup output

--- code/06.rb
+++ code/06.rb
@@ -42,11 +42,14 @@ lines.each do | line |
   nodes[p1] << p2
 end
 
-p santas_way = get_way_to_com("SAN", nodes).reverse
-p my_way =  get_way_to_com("YOU", nodes).reverse
-p meeting_point = (my_way & santas_way).last
+santas_way = get_way_to_com("SAN", nodes).reverse
+my_way =  get_way_to_com("YOU", nodes).reverse
+meeting_point = (my_way & santas_way).last
 
-p santas_way_to_meeting_point = get_way("SAN", meeting_point, nodes)
-p my_way_to_meeting_point = get_way("SAN", meeting_point, nodes)
+santas_way_to_meeting_point = get_way("SAN", meeting_point, nodes)
+my_way_to_meeting_point = get_way("YOU", meeting_point, nodes)
 
-p santas_way_to_meeting_point.size+my_way_to_meeting_point.size
+I_DIDNT_MANAGE_TO_CORRECTLY_READ_THE_PUZZLE_AND_THEREFORE_DIDNT_SEE_THAT_I_WASNT_SUPPOSED_TO_COUNT_THE_ACTUAL_DISTANCE_TO_SANTA_BUT_RATHER_HOW_MANY_ORBIT_JUMPS_IT_TAKES_TO_GET_INTO_THE_SAME_ORBIT_AS_HE_IS = 2
+orbits_to_jump = santas_way_to_meeting_point.size + my_way_to_meeting_point.size - I_DIDNT_MANAGE_TO_CORRECTLY_READ_THE_PUZZLE_AND_THEREFORE_DIDNT_SEE_THAT_I_WASNT_SUPPOSED_TO_COUNT_THE_ACTUAL_DISTANCE_TO_SANTA_BUT_RATHER_HOW_MANY_ORBIT_JUMPS_IT_TAKES_TO_GET_INTO_THE_SAME_ORBIT_AS_HE_IS
+
+puts "P2: #{orbits_to_jump}"
