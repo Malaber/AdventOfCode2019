c5b680ac4bb1269c6468e8cdd2038ab0d7b63628
Malaber <git@schaedler.rocks>
2019-12-06 10:20:40 +0100

    Add highly inefficient solution to 06/1

--- code/06.rb
+++ code/06.rb
@@ -1,23 +1,39 @@
 require_relative '../input'
 lines = get_lines $PROGRAM_NAME
 
-require 'rgl/dot'
-require 'rgl/adjacency'
 require 'set'
 
-graph = RGL::DirectedAdjacencyGraph.new
+def get_parent_node(node, nodes)
+  nodes.select {|n| nodes[n].include?(node)}.keys.first
+end
+
+def get_distance_from_com(node, nodes)
+  parent = get_parent_node(node, nodes)
+  if parent == "COM"
+    return 1
+  else
+    return 1 + get_distance_from_com(parent, nodes)
+  end
+end
+
 unique_nodes = Set.new
 
+nodes = {}
 lines.each do | line |
-  p1, p2 = line.split(')')
-  graph.add_edge(p1, p2)
+  p1, p2 = line.chomp.split(')')
+
   unique_nodes << p1
   unique_nodes << p2
+
+  nodes[p1] ||= []
+  nodes[p1] << p2
 end
 
-unique_nodes.each do | node |
-  # puts graph.dijkstra_shortest_path({}, "COM", node)
+distance = 0
+unique_nodes.each do |node|
+  unless node == "COM"
+    distance += get_distance_from_com(node, nodes)
+  end
 end
 
-graph.print_dotted_on
-graph.write_to_graphic_file
+puts distance
